aa708e000ea606b3ab7b46c5a2808a103be9d1ca
Arsh Bansal <arshbansal1111@gmail.com>
2026-06-10 14:33:20 +0530

    Implemented time limit and made problem for loop concurrent

--- main.go
+++ main.go
@@ -6,10 +6,14 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
 	csvFilenamePtr := flag.String("csv", "problems.csv", "A CSV file containing questions and answer in `question,answer` format.")
+	timeDurationPtr := flag.Int("limit", 30, "Time limit in which users must answer all questions.")
+	flag.Parse()
+
 	csvFile, err := os.Open(*csvFilenamePtr)
 	if err != nil {
 		exitWithErrMsg(fmt.Sprintf("CSV file not found: %s\n", *csvFilenamePtr))
@@ -22,18 +26,31 @@ func main() {
 
 	problemList := parseCsvLines(lines)
 	correctNum := 0
+	timer := time.NewTimer(time.Duration(*timeDurationPtr) * time.Second)
 	for ind, problem := range problemList {
 		fmt.Printf("Problem #%d: %s\n", ind+1, problem.question)
-		var response string
-		fmt.Scanf("%s\n", &response)
-		if response == problem.answer {
-			correctNum++
-			fmt.Println("Correct!")
-		} else {
-			fmt.Println("Wrong!")
+
+		responseCh := make(chan string)
+		go func() {
+			var response string
+			fmt.Scanf("%s\n", &response)
+			responseCh <- response
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Println("\nTime's up!")
+			fmt.Printf("\nYou got %d out %d questions correct.\n", correctNum, len(problemList))
+			return
+		case response := <-responseCh:
+			if response == problem.answer {
+				correctNum++
+				fmt.Println("Correct!")
+			} else {
+				fmt.Println("Wrong!")
+			}
 		}
 	}
-
 	fmt.Printf("\nYou got %d out %d questions correct.\n", correctNum, len(problemList))
 }
 
